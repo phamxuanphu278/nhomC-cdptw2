0fa9d037439acc35ea76487094a80b6a4492952f
NguyenTheBao23205 <43943189+NguyenTheBao23205@users.noreply.github.com>
2019-10-26 08:12:31 +0700

    modul-7

---
+++ NguyenTheBao_project_Vue/7/7.php
@@ -0,0 +1,39 @@
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+
+if (!class_exists('lessc')) {
+    $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+    require_once($dir_block . '/public/libs/lessc.inc.php');
+}
+
+$less = new lessc;
+$less->compileFile('public/less/7.less', 'public/less/7.css');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+    <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" type="text/css" href="<?php echo $url_path ?>/public/css/font-awesome.min.css">
+            <link rel="stylesheet" type="text/css" href="<?php echo $url_path ?>/public/css/bootstrap.min.css">
+            <link rel="stylesheet" type="text/css" href="<?php echo $url_path ?>/public/less/7.css">
+            <!-- <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.9.0/less.min.js"></script> -->
+            <script src="<?php echo $url_path ?>/public/js/jquery-3.1.1.min.js"></script>
+            <script src="<?php echo $url_path ?>/public/js/bootstrap.min.js"></script>
+            <title>Modul 7</title>
+    </head>
+
+    <body>
+        <?php include $dir_block . '/7-content.php'; ?>
+    </body>
+
+</html>
