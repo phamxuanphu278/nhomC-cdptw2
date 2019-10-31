1f524ab4f57beb6c8cf521c968125dfb1f8f07e3
phamxuanphu278 <phuphamxuan278@gmail.com>
2019-10-31 20:57:28 +0700

    Updates

---
+++ precache-manifest.37c94c18b91678eaa4b922d54e47c775.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d361e9a063db114670d9e575707a53bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "632b9c889903aa27ec42",
+    "url": "/static/css/2.400f043a.chunk.css"
+  },
+  {
+    "revision": "71b735b8bf9a7c1e56bf",
+    "url": "/static/css/main.164a8fb7.chunk.css"
+  },
+  {
+    "revision": "632b9c889903aa27ec42",
+    "url": "/static/js/2.9b0b2c59.chunk.js"
+  },
+  {
+    "revision": "71b735b8bf9a7c1e56bf",
+    "url": "/static/js/main.54471ee2.chunk.js"
+  },
+  {
+    "revision": "d10f44147db69f91b4f1",
+    "url": "/static/js/runtime-main.56c3c331.js"
+  },
+  {
+    "revision": "a07644473cab9851db602fd24c0c747a",
+    "url": "/static/media/2.a0764447.jpg"
+  },
+  {
+    "revision": "a57c9f20160f5a3568d78a5538340b41",
+    "url": "/static/media/about.a57c9f20.PNG"
+  },
+  {
+    "revision": "4ce728ee1488e24eed5453c42ff9ec62",
+    "url": "/static/media/adidas1.4ce728ee.jpg"
+  },
+  {
+    "revision": "3f11c2ef6377ad01034e04c2c12d62d0",
+    "url": "/static/media/adidas2.3f11c2ef.jpg"
+  },
+  {
+    "revision": "feae4b0e80ca2d646000959b0df1d70e",
+    "url": "/static/media/adidas3.feae4b0e.jpg"
+  },
+  {
+    "revision": "b3d9e6fb44d4b716f2c2a10b3f1c43df",
+    "url": "/static/media/adidas4.b3d9e6fb.png"
+  },
+  {
+    "revision": "94e0ac492442537facb29953decedc80",
+    "url": "/static/media/chuki.94e0ac49.PNG"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "514e042de722825297ff0447cbfa7cd0",
+    "url": "/static/media/giay1.514e042d.jpg"
+  },
+  {
+    "revision": "c9999bc502ef1302760ee485d87b150e",
+    "url": "/static/media/giay1.c9999bc5.jpg"
+  },
+  {
+    "revision": "e6d7e6f1e1314edc1b85ed4322e55fc6",
+    "url": "/static/media/giay1.e6d7e6f1.png"
+  },
+  {
+    "revision": "6b01625daf9763269fdec9d0dc92797e",
+    "url": "/static/media/giay2.6b01625d.jpg"
+  },
+  {
+    "revision": "ac9b0af6842fe61018199c058da5614f",
+    "url": "/static/media/giay2.ac9b0af6.jpg"
+  },
+  {
+    "revision": "b98711e3ad91a1bea5f622902e4de7d8",
+    "url": "/static/media/giay2.b98711e3.jpg"
+  },
+  {
+    "revision": "166521280f802e913dca0c28e40bb7a7",
+    "url": "/static/media/giay3.16652128.jpg"
+  },
+  {
+    "revision": "7aa1f1a0507fd0b14b9d801f82e1a453",
+    "url": "/static/media/giay3.7aa1f1a0.jpg"
+  },
+  {
+    "revision": "6b01625daf9763269fdec9d0dc92797e",
+    "url": "/static/media/giay4.6b01625d.jpg"
+  },
+  {
+    "revision": "35b003f4a2610f2ebf57338aa290ecbf",
+    "url": "/static/media/giay5.35b003f4.jpg"
+  },
+  {
+    "revision": "118049649c24cb934bb66a6169e330e2",
+    "url": "/static/media/hoa.11804964.jpg"
+  },
+  {
+    "revision": "9f12de6ddf46d79a503186d74ac80e94",
+    "url": "/static/media/icon1.9f12de6d.jpg"
+  },
+  {
+    "revision": "c7de68fdd4a26dd65d1ee219dd086479",
+    "url": "/static/media/icon2.c7de68fd.png"
+  },
+  {
+    "revision": "b66a694a9f79c9198cd435af102db5bc",
+    "url": "/static/media/logo.b66a694a.png"
+  },
+  {
+    "revision": "42c5afc200a4edf6a30bd89fad5a1879",
+    "url": "/static/media/logo1.42c5afc2.png"
+  },
+  {
+    "revision": "56b78e073b9971a8580b80bbee2761fc",
+    "url": "/static/media/master.56b78e07.png"
+  },
+  {
+    "revision": "40ed1cd177b39bb07b0aff817a5ac81f",
+    "url": "/static/media/puma1.40ed1cd1.jpg"
+  },
+  {
+    "revision": "fbd6ca8a536296ac767a39f2e86c9a15",
+    "url": "/static/media/puma2.fbd6ca8a.jpg"
+  },
+  {
+    "revision": "5e8e0d4f1a002613aebce53587a08a34",
+    "url": "/static/media/quan2.5e8e0d4f.jpg"
+  },
+  {
+    "revision": "9ccb3ce1effea10022923865d4108be0",
+    "url": "/static/media/quan3.9ccb3ce1.jpg"
+  },
+  {
+    "revision": "e93778fa83eb8bcfbfa173ef0ac44cfd",
+    "url": "/static/media/quan4.e93778fa.jpg"
+  }
+]);
